f8ea447c65ce5906002f991ecfb477e9a0812da2
Syl202309 <lsy1109929@Gmail.com>
2024-01-09 22:28:11 +0100

    upload link

--- mySketch.js
+++ mySketch.js
@@ -1,7 +1,10 @@
 let uploadedImage;
+let saturation;
+let img;
+
 
 var chain, dc, sc, doubleDC;
-var puff, dcCorner, shell, picot, dcChainCorner;
+var puff, dcChCorner, shell;
 
 var sliderStitch, sliderStitchY, sliderStitchOver;
 var sliderRound, sliderRoundY, sliderRoundOver;
@@ -25,14 +28,12 @@ var decoration, pattern;
 var singleIncrease, doubleIncrease;
 
 var randomize = false; //booleans that detect if the button is pressed
-var normal = true;
 var checked = false;
-var helper = false;
-//var generate = false;
-var randomFill = 255, normalFill = 0, generateFill = 200, generateTextFill = 0, randomTextFill = 0, normalTextFill = 255;
+var generate = false;
+var randomFill = 255,  generateFill = 200, generateTextFill = 0, randomTextFill = 0;
 
 var n = 6;
-var initStitchNum = 6;
+var initStitchNum;
 var equivInitStitchNum;
 var saveNum = 1;
 var incCounter; // keeps track of the number of increase rounds
@@ -47,20 +48,14 @@ var r = 250;
 
 var font;
 
-var crochetEx;
-
 function preload() {
   chain = loadImage("stitches/chain.svg");
   dc = loadImage("stitches/dc.svg");
   sc = loadImage("stitches/sc.svg");
   doubleDC = loadImage("stitches/double_dc.svg");
-  dcChainCorner = loadImage("stitches/dc_3ch_corner.svg");
-  dcCorner = loadImage("stitches/dc_ch_corner.svg");
+  dcChCorner = loadImage("stitches/dc_ch_corner.svg");
   shell = loadImage("stitches/shell.svg");
-  picot = loadImage("stitches/picot.svg");
   puff = loadImage("stitches/puff.svg");
-  
- // crochetEx = loadImage("crochet-01.png");
   font = loadFont("Raleway-Light.ttf");
 }
 
@@ -72,11 +67,9 @@ function setup() {
   stitches[1] = sc;
   stitches[2] = dc;
   stitches[3] = doubleDC;
-  stitches[4] = dcCorner;
-  stitches[5] = dcChainCorner;
-  stitches[6] = shell;
-  stitches[7] = picot;
-  stitches[8] = puff;
+  stitches[4] = dcChCorner;
+  stitches[5] = shell;
+  stitches[6] = puff;
   
   basicStitches[0] = chain;
   basicStitches[1] = sc;
@@ -84,10 +77,8 @@ function setup() {
   basicStitches[3] = doubleDC;
   basicStitches[4] = puff;
   
-  specialStitches[0] = dcCorner;
-  specialStitches[1] = dcChainCorner;
-  specialStitches[2] = shell;
-  specialStitches[3] = picot;
+  specialStitches[0] = dcChCorner;
+  specialStitches[1] = shell;
   
   sliderStart = width-300;
   sliderEnd = width-20;
@@ -98,24 +89,29 @@ function setup() {
   
   nobSize = 20;
   
-  stitchNames = ["chain", "single crochet", "double crochet", "two double crochet", "dc chain shell",
-"three chain dc shell", "shell", "picot", "puff"];
-  equivNum = [1, 1, 1, 2, 2, 2, 5, 1, 1];
+  stitchNames = ["chain", "single crochet", "double crochet", "two double crochet", "dc chain corner", "shell", "puff"];
+  equivNum = [1, 1, 1, 2, 2, 5, 1];
   basicEquivNum = [1, 1, 1, 2, 1];
-  specialEquivNum = [2, 2, 5, 1];
+  specialEquivNum = [2, 5];
   basicStitchesNames = ["chain", "single crochet", "double crochet", "two double crochet", "puff"];
-  specialStitchesNames = ["dc chain shell", "three chain dc shell", "shell", "picot"];
+  specialStitchesNames = ["dc chain corner", "shell"];
    
   imageMode(CENTER);
   noLoop();
 
    // Create a file input element
    let fileInput = createFileInput(handleFile);
-   fileInput.position(width -200, height - 200);
+   fileInput.position(60, height/2 - 60);
+   fileInput.style('width', '200px');
+   fileInput.style('height', '30px');
+   fileInput.style('color', '#ffffff'); // Text color
+   fileInput.style('background-color', '#cccccc'); // Background color
+   fileInput.style('border', '2px solid #555555'); // Border style
+   fileInput.style('border-radius', '5px'); // Border radius
 }
 
 function draw() {
-  clear();
+  // clear();
   background(255);
   incCounter = 1;
 
@@ -126,108 +122,59 @@ function draw() {
 
   rectMode(CENTER);
   fill(randomFill);
-  rect(150, 125, 200, 50); //random and normal buttons
-  fill(normalFill);
-  rect(150, 200, 200, 50);
-  fill(generateFill); // generate button
-  rect(150, 400, 200, 50);
+  rect(150, 125, 200, 50); //generate button
 
-  if (normal) {
-    fill(255);
-    rect(62.5, 250, 25, 25);
-    if (checked) {
-      fill(0);
-      rect(62.5, 250, 15, 15);
-    }
-  }
 
   noStroke();
   textAlign(CENTER);
   textSize(20);
   textFont(font);
   fill(randomTextFill);
-  text("RANDOMIZE", 150, 132); // buttons to control the output
-  fill(normalTextFill);
-  text("NORMAL", 150, 207);
-  fill(generateTextFill);
-  text("GENERATE", 150, 407);
-
-
-
-  if (normal) {
-    fill(0);
-    text("Decoration Round", 165, 257.5);
-  }
-
-  ellipseMode(CENTER); // helper icon
-  noFill();
-  stroke(0);
-  ellipse(62.5, 7 * height / 8, 30, 30);
-  noStroke();
-  fill(0);
-  text("?", 62.5, 7 * height / 8 + 7);
-
-  //helper text
-  if (helper) {
-    fill(0);
-    textAlign(LEFT);
-    textSize(14);
-    rectMode(CORNER);
-    text("GENERATE - Generate pattern.\nUse the sliders to change the number of rounds and stitches.\n\"S\" - save pattern (prompts download).\nRANDOM - generates random stitches for each round.\nNORMAL - follows normal increasing rounds.\nDecoration Round - adds special stitch to the last round of a normal pattern.", 90, 640, width / 2, 570);
-    rectMode(CENTER);
-  }
+  text("GENERATE", 150, 132); // buttons to control the output
 
   //slider
   stroke(0);
-  line(sliderStart, sliderStitchY, sliderEnd, sliderStitchY);
   line(sliderStart, sliderRoundY, sliderEnd, sliderRoundY);
-  noStroke();
-  rect(sliderStitch.x, sliderStitch.y, nobSize, nobSize);
+  //noStroke();
   rect(sliderRound.x, sliderRound.y, nobSize, nobSize);
 
   textAlign(LEFT);
   textSize(20);
-  initStitchNum = int(map(sliderStitch.x, sliderStart, sliderEnd, 4, 8));
-  n = int(map(sliderRound.x, sliderStart, sliderEnd, 3, 10));
-  text("Number of stitches: " + initStitchNum, sliderStart - 10, sliderStitchY - 20);
-  text("Number of rounds: " + n, sliderStart - 10, sliderRoundY - 20);
+  n = int(map(sliderRound.x, sliderStart, sliderEnd, 3, 9));
+  text("Number of rounds: " + n, sliderStart, sliderRoundY - 30);
 
   // making the crochet chart
   noFill();
   noStroke();
+  let hueAnalysis = analyzeHSL(img);
+  averageLightness = hueAnalysis.lightness.average;
+  let mappedInitStitchNum = int(map(averageLightness, 0, 100, 7, 3));
+  displayColorSquare(hueAnalysis.hue.average, hueAnalysis.saturation.average, hueAnalysis.lightness.average);
   push();
   translate(width / 2, height / 2);
+  
+  // let hueAnalysis = analyzeHSL(img);
+  // averageLightness = hueAnalysis.lightness.average;
+  // let mappedInitStitchNum = int(map(averageLightness, 0, 100, 7, 3));
 
   for (var i = 1; i <= n; i++) { // makes the rounds with invisible circles
     stitchInd = round(random(0, stitches.length - 1));
-    stitchIndTwo = round(random(0, stitches.length - 1));
     basicStitchInd = round(random(0, basicStitches.length - 1));
     specialStitchInd = round(random(0, specialStitches.length - 1));
-    incNum = i * initStitchNum; // incNum is the number of stitches in each round, increases each time (5, 10, 15, etc)
+    incNum = i * mappedInitStitchNum; // incNum is the number of stitches in each round, increases each time
 
-    stitchNum = incNum * equivNum[stitchInd];
-
-    if (i == 1) {
-      equivInitStitchNum = initStitchNum;
-      incChange = equivNum[stitchInd] * equivInitStitchNum;
-    } else {
-      equivInitStitchNum = incChange;
-      incChange = incChange * equivNum[stitchInd];
-    }
-
-    if (normal && !checked) {
-      doubleIncrease = new CrochetRound(basicStitches[2], basicStitches[3]);
-      doubleIncrease.increaseRound(incNum, i * r, n);
-    } else if (normal && checked) {
-      doubleIncrease = new CrochetRound(basicStitches[2], basicStitches[3]);
-      doubleIncrease.increaseRound(incNum, i * r, n);
-      if (i == n) {
-        decoration = new CrochetRound(specialStitches[specialStitchInd], specialStitches[specialStitchInd]);
-        decoration.repeatAround(incNum, (i + 1) * r, n);
+    if (randomize) {
+      let hueAnalysis = analyzeHSL(img);
+      averageSaturation = hueAnalysis.saturation.average;
+  
+      chainRound = new CrochetRound(basicStitches[basicStitchInd], specialStitches[specialStitchInd]);
+      if (averageSaturation <= 50) {
+          chainRound.repeatAround(incNum, i * r);
+          console.log('repeat');
+      }  else {
+            chainRound.patternStitch(incNum, i * r);
+            console.log('pattern');
       }
-    } else if (randomize) {
-      chainRound = new CrochetRound(stitches[stitchInd], stitches[stitchIndTwo]);
-      chainRound.repeatAround(incNum, i * r);
       pattern = "Round " + i + ": " + incNum + " " + stitchNames[stitchInd] + " stitches\n";
       fill(0);
       textAlign(LEFT);
@@ -237,55 +184,27 @@ function draw() {
       noFill();
     }
 
+  
     rectMode(CENTER);
 
     incCounter++;
   }
   pop();
-
-
-  if (uploadedImage) {
-  
-    image(uploadedImage, width / 2 - uploadedImage.width / 2, height / 2 - uploadedImage.height / 2);
-  }
+  fill(0);
+  textAlign(LEFT);
+  text("Upload an image to get ideas!\nPress Generate to get different designs\nTry images with different saturation and lightness.\nRefresh webpage before uploading new images.\nUse the sliders to change the number of rounds.\n - Press 'S' to save pattern.\n",50, height/2 - 200);
+  text("Color suggestions:", 60, height-135);
 
 }
 
-
-
-
-//Callback function to handle the uploaded file
-// function handleFile(file) {
-//   if (file.type === 'image') {
-//     // Load the image and assign it to the uploadedImage variable
-//     uploadedImage = loadImage(file.data, (img) => {
-//       // Resize the image to fit within the canvas
-//       let aspectRatio = img.width / img.height;
-//       let maxDimension = min(width/4, height/4);
-//       if (img.width > img.height) {
-//         img.resize(maxDimension, maxDimension / aspectRatio);
-//       } else {
-//         img.resize(maxDimension * aspectRatio, maxDimension);
-//       }
-
-//       // Create an img element in the HTML body to display the image
-//       let imgElement = createImg(file.data);
-//       imgElement.size(uploadedImage.width, uploadedImage.height);
-//       imgElement.position(width - uploadedImage.width / 2 -200 , height / 2 - uploadedImage.height / 2);
-//     });
-//   } else {
-//     // If the file is not an image, show an error message
-//     console.error('Invalid file type. Please upload an image.');
-//   }
-// }
-
-// Callback function to handle the uploaded file
 // Callback function to handle the uploaded file
 function handleFile(file) {
+  
   if (file.type === 'image') {
     // Load the image and assign it to the uploadedImage variable
-    uploadedImage = loadImage(file.data, (img) => {
+    uploadedImage = loadImage(file.data, (loadedImg) => {
       // Resize the image to fit within the canvas
+      img = loadedImg;
       let aspectRatio = img.width / img.height;
       let maxDimension = min(width / 4, height / 4);
       if (img.width > img.height) {
@@ -297,29 +216,62 @@ function handleFile(file) {
       // Create an img element in the HTML body to display the image
       let imgElement = createImg(file.data, 'uploaded image', ''); // Updated line
       imgElement.size(uploadedImage.width, uploadedImage.height);
-      imgElement.position(width - uploadedImage.width / 2 - 200, height / 2 - uploadedImage.height / 2);
-
-      // Count color density after displaying the image
-      let colorDensity = countColorDensityByRanges(img);
-      console.log('Color Density:', colorDensity);
-    });
+      imgElement.position(60, height / 2);
+      
+      let hueAnalysis = analyzeHSL(img);
+      averageSaturation = hueAnalysis.saturation.average;
+      displayColorSquare(hueAnalysis.hue.average, hueAnalysis.saturation.average, hueAnalysis.lightness.average);
+      console.log('HSL Analysis:', hueAnalysis);
+      
+    });  
+    
   } else {
     // If the file is not an image, show an error message
     console.error('Invalid file type. Please upload an image.');
   }
+
+  
 }
 
-function countColorDensityByRanges(img) {
+function rgbToHsl(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    let d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+  return [h, s, l];
+}
+
+function analyzeHSL(img) {
+
+  if (!img || !img.width || !img.height) {
+    return {
+      hue: { average: 0 },
+      saturation: { average: 0 },
+      lightness: { average: 0 }
+    };
+  }
+
   img.loadPixels();
-  let totalPixels = img.pixels.length / 4; // 4 values per pixel (R, G, B, A)
-
-  let redRange = { min: 100, max: 255 }; // Example red range
-  let greenRange = { min: 0, max: 150 };  // Example green range
-  let blueRange = { min: 0, max: 150 };   // Example blue range
-
-  let countRed = 0;
-  let countGreen = 0;
-  let countBlue = 0;
+  let totalPixels = img.pixels.length / 4; 
+  let lightnessValues = [];
+  let hueValues = [];
+  let saturationValues = [];
 
   for (let i = 0; i < totalPixels; i++) {
     let pixelIndex = i * 4;
@@ -327,27 +279,48 @@ function countColorDensityByRanges(img) {
     let pixelG = img.pixels[pixelIndex + 1];
     let pixelB = img.pixels[pixelIndex + 2];
 
-    // Check if the pixel color is within the defined ranges
-    if (pixelR >= redRange.min && pixelR <= redRange.max) {
-      countRed++;
-    }
+    let hsl = rgbToHsl(pixelR, pixelG, pixelB);
+    let pixelLightness = hsl[2] * 100; 
+    let pixelHue = hsl[0] * 360;
+    let pixelSaturation = hsl[1] * 100; 
 
-    if (pixelG >= greenRange.min && pixelG <= greenRange.max) {
-      countGreen++;
-    }
-
-    if (pixelB >= blueRange.min && pixelB <= blueRange.max) {
-      countBlue++;
-    }
+    lightnessValues.push(pixelLightness);
+    hueValues.push(pixelHue);
+    saturationValues.push(pixelSaturation);
   }
 
-  return { countRed, countGreen, countBlue };
+  let averageHue = hueValues.reduce((acc, val) => acc + val, 0) / hueValues.length;
+  let averageSaturation = saturationValues.reduce((acc, val) => acc + val, 0) / saturationValues.length;
+  let averageLightness = lightnessValues.reduce((acc, val) => acc + val, 0) / lightnessValues.length;
+
+  return {
+    hue: { average: averageHue },
+    saturation: { average: averageSaturation },
+    lightness: { average: averageLightness }
+  };
 }
 
+function displayColorSquare(hue, saturation, lightness) {
+  // Convert HSL to HSB
 
+  colorMode(HSB, 360, 100, 100);
+  let hsbColor = color(hue, saturation, lightness);
+  let hsbColor1 = color(hue+60, saturation+50, lightness+20);
+  let hsbColor2 = color(hue-20, saturation+100, lightness+80);
+  let hsbColor3 = color(hue+120, saturation, lightness+30);
 
-
-
+  // Display color square
+  fill(hsbColor);
+  rect(235, height-100, 50, 50); // You can adjust the size of the square
+  fill(hsbColor1);
+  rect(185, height-100, 50, 50);
+  fill(hsbColor2);
+  rect(135, height-100, 50, 50);
+  fill(hsbColor3);
+  rect(85, height-100, 50, 50);
+  // Reset color mode to default (RGB)
+  colorMode(RGB, 255);
+}
 
 // Control functions
 function mousePressed() {
@@ -356,45 +329,10 @@ function mousePressed() {
   
   if (mouseX > 50 && mouseX < 250 && mouseY > 100 && mouseY < 150) { // randomize
     randomize = true;
-    normal = false;
     randomFill = 0;
     randomTextFill = 255; 
-    normalFill = 255;
-    normalTextFill = 0;
     redraw();
-  }
-  else if (mouseX > 50 && mouseX < 250 && mouseY > 175 && mouseY < 225) { // normal
-    randomize = false;
-    normal = true;
-    randomFill = 255;
-    randomTextFill = 0; 
-    normalFill = 0;
-    normalTextFill = 255;
-    redraw();
-  }
-  else if (mouseX > 55 && mouseX < 70 && mouseY > 242.5 && mouseY < 257.5) { // normal decoration
-    if(checked) {
-      checked = false;
-      redraw();
-    }
-    else {
-      checked = true;
-      redraw();
-    }
-  }
-  //    rect(150, 400, 200, 50);
-  else if (mouseX > 50 && mouseX < 250 && mouseY > 375 && mouseY < 425) { // generate button
-    redraw();
-  }
-
-  
-  var d = dist(mouseX, mouseY, 62.5, 7*height/8);
-  if (d < 15 && !helper) {
-    helper = true;
-    redraw();
-  }
-  else if (d < 15 && helper) {
-    helper = false;
+  }  else if (mouseX > 50 && mouseX < 250 && mouseY > 375 && mouseY < 425) { // generate button
     redraw();
   }
 }
@@ -448,8 +386,6 @@ function checkHover() {
     sliderRoundOver = false;
   }
 }
-
-
 
 function keyTyped() {
   if (key === 's') {
